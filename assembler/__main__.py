0ea99e088ee3c92b250968937f4d42e556a90a13
Jonathan McCall <jonathan.mccall@hbo.com>
2017-06-30 11:03:10 -0400

    Swap filename extensions

--- assembler/__main__.py
+++ assembler/__main__.py
@@ -2,7 +2,7 @@ import sys
 import parser
 
 in_filename = sys.argv[1]
-out_filename = sys.argv[1].rpartition('.')[0] + '.asm'
+out_filename = sys.argv[1].rpartition('.')[0] + '.hack'
 
 in_file = open(in_filename)
 out_file = open(out_filename, 'w')
