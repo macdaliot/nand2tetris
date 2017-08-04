2ef8e812d1c5ea96dc88dd0c52d97c7f6ba2efb2
Jonathan McCall <jonathan.mccall@hbo.com>
2017-08-04 09:38:18 -0400

    Modify vm_translator to handle multiple src files

--- vm_translator/__main__.py
+++ vm_translator/__main__.py
@@ -1,13 +1,23 @@
+import os
 import sys
 from parser import Parser
 from code_writer import CodeWriter
 
-in_file = sys.argv[1]
+vm_file = sys.argv[1]
 
-writer = CodeWriter(in_file + '.asm')
+writer = CodeWriter(vm_file + '.asm')
 
-with open(in_file) as f:
-    writer.set_filename(in_file.split('.')[0])
-    parser = Parser(f, writer)
-    for instr in parser.parse():
-        print instr
+path = ''
+if os.path.isdir(vm_file):
+    path = vm_file
+    vm_files = os.listdir(vm_file)
+else:
+    vm_files = [vm_file]
+
+for vm_file in vm_files:
+    print 'parsing:', vm_file
+    with open(os.path.join(path, vm_file)) as f:
+        writer.set_filename(vm_file.split('.')[0])
+        parser = Parser(f, writer)
+        for instr in parser.parse():
+            print instr
